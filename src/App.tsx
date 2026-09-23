b7e28967c1d6ebc915fe7b0e2104a579c2358d82
Elioth Krahler <eliothkrahler@gmail.com>
2026-09-22 19:42:55 -0500

    Tighten the PhD line in the hero

--- src/App.tsx
+++ src/App.tsx
@@ -39,8 +39,8 @@ export default function App() {
                                     Kubernetes, Terraform and a Java API to
                                     build this site. Each one is shipped and
                                     written up, including what broke. That same
-                                    curiosity is why I plan to do a PhD in
-                                    physics.
+                                    curiosity is why I'll eventually get my PhD
+                                    in physics.
                                 </p>
                             </header>
                             {/** <ForecastHero /> */}
